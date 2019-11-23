c5cee1a7b222343e8a2d7e803c7a1759b2ace1a0
toriwat328 <toriwat328@gmail.com>
2019-11-23 13:20:15 -0500

    added countdown to christmas

--- public/js/app.js
+++ public/js/app.js
@@ -239,3 +239,40 @@ this.getWishlist();
 
 
 }]) //closes controller
+
+var timer;
+
+var compareDate = new Date('December 25, 2019 23:15:30');
+compareDate.setDate(compareDate.getDate()); 
+
+timer = setInterval(function() {
+  timeBetweenDates(compareDate);
+}, 1000);
+
+function timeBetweenDates(toDate) {
+  var dateEntered = toDate;
+  var now = new Date();
+  var difference = dateEntered.getTime() - now.getTime();
+
+  if (difference <= 0) {
+
+    // Timer done
+    clearInterval(timer);
+
+  } else {
+
+    var seconds = Math.floor(difference / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+
+    hours %= 24;
+    minutes %= 60;
+    seconds %= 60;
+
+    $("#days").text(days);
+    $("#hours").text(hours);
+    $("#minutes").text(minutes);
+    $("#seconds").text(seconds);
+  }
+}
